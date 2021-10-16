a713d21cf2fa0e8a49ded8a4ff94656a829a4d94
thundersen <thundersen@posteo.de>
2021-10-16 16:59:45 +0200

    deploy locally to vevantzedata subdir

--- deploy/deploy_locally.sh
+++ deploy/deploy_locally.sh
@@ -3,7 +3,9 @@
 NET_VERSION=`grep -oPm1 "(?<=<TargetFramework>)[^<]+" collector/Collector.csproj`
 PROJECT_DIR=`dirname $0`/../collector
 DLL_DIR=${PROJECT_DIR}/bin/Debug/${NET_VERSION}
-TARGET_DIR=~/.steam/steam/steamapps/common/Timberborn/BepInEx/plugins
+TARGET_DIR=~/.steam/steam/steamapps/common/Timberborn/BepInEx/plugins/vevantzedata
+
+mkdir -p ${TARGET_DIR}
 
 cp -v ${DLL_DIR}/VeVantZeData.dll ${TARGET_DIR}
 cp -v ${DLL_DIR}/InfluxDB.Client*.dll ${TARGET_DIR}
